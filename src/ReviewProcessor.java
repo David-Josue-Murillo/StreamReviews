b954e7317ecf9421875eb2d03c7e75ebc67d00a1
David Murillo <david.josue.murillo23@gmail.com>
2025-09-29 15:23:34 -0500

    Agregando la clase ReviewProcessor

---
+++ src/ReviewProcessor.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ReviewProcessor {
+    private static final List<String> PALABRAS_POSITIVAS = Arrays.asList(
+            "excelente", "recomendado", "muy bueno", "me encanto", "me gusto", "satisfecho", "perfecto", "fantastico", "increible", "maravilloso"
+    );
+
+    private List<Review> reviews;
+
+    public ReviewProcessor(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+}
