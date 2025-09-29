1eb95ede9747845df539712d73e0363b6270ca26
David Murillo <david.josue.murillo23@gmail.com>
2025-09-29 14:20:09 -0500

    Agregando la clase Producto con los elementos definidos

---
+++ src/Producto.java
@@ -0,0 +1,14 @@
+public enum Producto {
+    COMPUTADORA,
+    MOUSE,
+    TECLADO,
+    MONITOR,
+    IMPRESORA,
+    TABLET,
+    CELULAR,
+    AURICULARES,
+    ALTAVOCES,
+    CAMARA,
+    USB,
+    OTRO
+}
